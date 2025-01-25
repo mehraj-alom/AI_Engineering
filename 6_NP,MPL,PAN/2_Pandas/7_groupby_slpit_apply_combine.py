e159be5b81dafaa58c0598947abc05ec58735665
Mehraj Alom Tapadar <mehraj.official000@gmail.com>
2025-01-26 00:11:35 +0530

    Groupby

--- 6_NP,MPL,PAN/2_Pandas/7_groupby_slpit_apply_combine.py
+++ 6_NP,MPL,PAN/2_Pandas/7_groupby_slpit_apply_combine.py
@@ -10,3 +10,15 @@ print("\n")
 print("3__\n",x.max(),"\n")
 print("4__\n",x.mean(numeric_only=True),"\n")
 print("5__\n",x.describe(),"\n")
+
+print("\n__",df.shape,"\n")
+
+for city,city_df in x :
+    print(city)
+    if city == "New York":
+        print("Mean Windspeed is ",city_df["Windspeed (mph)"].mean())
+    print(city_df)
+# getgroup methood 
+print("5__\n",x.get_group("Los Angeles"))
+
+print("6__\n",x.max(),"\n")
